4da9e5455e603240a301cd5af6c70b833ef3cf89
John Larsen <john@pcis.se>
2016-09-01 20:29:49 +0200

    SHA class for hashing files smal change to upsateserver class fro random string generation

---
+++ Chared/RandomStringGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Chared
+{
+    public static class RandomStringGenerator
+    {
+        private static Random random;
+        const string UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+        const string NUMBERS = "0123456789";
+        const string SYMBOLS = @"~`!@#$%^&*()-_=+<>?:,./\[]{}|'";
+
+        static RandomStringGenerator()
+        {
+            random = new Random();
+        }
+        public static string NextString(int length)
+        {
+            return StringGenerator(length);
+        }
+
+        private static string StringGenerator(int length, bool lowerCase = true, bool upperCase = true, bool numbers = true, bool symbols = true)
+        {
+            char[] charArray = new char[length];
+            string charPool = string.Empty;
+
+            if (lowerCase) charPool += LOWERCASE;
+            if (upperCase) charPool += UPPERCASE;
+            if (numbers) charPool += NUMBERS;
+            if (symbols) charPool += SYMBOLS;
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                int index = random.Next(0, charPool.Length);
+                charArray[i] = charPool[index];
+            }
+            return new string(charArray);
+        }
+    }
+}
--- Chared/UpdateServer.cs
+++ Chared/UpdateServer.cs
@@ -11,5 +11,6 @@ namespace Chared
         public string Name { get; set; }
         public string Port { get; set; }
         public string Ip { get; set; }
+        public string CertDomain { get; set; }
     }
 }
