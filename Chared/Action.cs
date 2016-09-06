b269221d13c6e7ebd0968a7b303202a6537a8488
john larsen <john@pcis.se>
2016-09-06 15:56:00 +0200

    Added stop and start application for Action

--- Chared/Action.cs
+++ Chared/Action.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace Chared
 {
-    class Action
-    {
-        public string Name { get; set; }
-        public string FilePath { get; set; }
-        public string When { get; set; }//TODO make this enum
-        public string Args { get; set; }
-        public string WorkingDir { get; set; }
-        public string StartStop { get; set; } //TODO enum or bool ? 
-    }
+	class Action
+	{
+		public string Name { get; set; }
+		public string FilePath { get; set; }
+		public string FileName { get; set;  }
+		public string When { get; set; }//TODO make this enum
+		public string Args { get; set; }
+		public string WorkingDir { get; set; }
+		public string StartStop { get; set; } //TODO enum or bool ? 
+        public bool WaiteForExit { get; set; }
+        public int TimeToWaitForExit { get; set; } = 0; 
+	}
 }
--- Chared/ActionExecute.cs
+++ Chared/ActionExecute.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Chared
-{
-    //TODO DRAFT 
+{    
     class ActionExecute
-    {
+    {        
+        /// <summary>
+        /// Executs the specified actions in  list.
+        /// 
+        /// NOTE if execute fail do we whant to stop update  by returning false  ?  
+        /// </summary>
+        /// <param name="actionList">The action list.</param>
+        /// <param name="when">The when. to execute </param>
+        /// <returns>bool true or false</returns>
         public bool Execut(List<Action> actionList, string when)
         {
             if (actionList != null && actionList.Count > 0)
@@ -19,11 +24,11 @@ namespace Chared
                     {
                         if (action.StartStop == "start")
                         {
-                            //TODO call a function to start here 
+                            if (!ActionStart(action)) return false; 
                         }
                         else
                         {
-                            //TODO call a function to stop here 
+                            if (!ActionStop(action)) return false; 
                         }
                     }
                     return false; 
@@ -32,19 +37,72 @@ namespace Chared
             return false; 
         }
 
+
+        /// <summary>
+        /// Start Action and it set wait for it exit 
+        /// NOTE:  
+        ///  Do we need properties in action for 
+        ///  * ProcessWindowsStyle
+        ///  * CreateNoWindow  true / false
+        ///  * USE shell execute 
+        /// 
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns>Bool true or false </returns>
         private bool ActionStart(Action action)
-        {
-            // TODO add code to start action 
-            return false;
+        {           
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.WorkingDirectory = @action.WorkingDir;
+            start.Arguments = action.Args;
+            start.FileName = @action.FilePath;
+            try
+            {
+                var proc = Process.Start(start);
+                if(action.WaiteForExit)
+                {
+                    proc.WaitForExit(action.TimeToWaitForExit);
+                }
+                return true; 
+            }
+            catch(Exception e)
+            {
+                return false; 
+            }     
         }
 
+        /// <summary>
+        /// Actions stop program.
+        /// First try to close program gracefully wait 5 sec for it to exit 
+        /// If program is running try to kill it 
+        /// If kill doesn't work we cant stop the program return false
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns>If program is stoped return true else return false</returns>
         private bool ActionStop(Action action)
-        {
-            //TODO add code to stop action 
-            return false; 
+        {           
+            Process[] workers = Process.GetProcessesByName(action.FileName.Replace(".exe","").Replace(".EXE", ""));  
+            foreach(var worker in workers)
+            {
+                try
+                {                    
+                    worker.CloseMainWindow();
+                    worker.WaitForExit(5000);                    
+                    if (!worker.HasExited)
+                    {                        
+                        worker.Kill();
+                        worker.WaitForExit(5000); 
+                        if(!worker.HasExited)
+                        {                            
+                            return false; 
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    return false; 
+                }
+            }
+            return true; 
         }
     }
-
-
-    
 }
