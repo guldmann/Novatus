b0a9166b1e52ab1fd5b2a5850345b6453e27f351
john larsen <john@pcis.se>
2016-09-07 22:43:47 +0200

    changes to versioninfo  properties

--- Chared/VersionInfo.cs
+++ Chared/VersionInfo.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace Chared
 {
-    class VersionInfo
+    public class VersionInfo
     {
-        string versionNumber { get; set; }
+		public string ApplicationName { get; set; }
+        string ApplicationVersion { get; set; }
         string VersionInfoMSG { get; set; }
-    }
+		List<RegKey> RegKeyList { get; set; }
+		public string ContentFileName { get; set; }
+		public string TempDir { get; set; }
+		List<UpdateServer> ServerList { get; set; }
+		
+	}
 }
--- NovatusDistributionClient/Main.cs
+++ NovatusDistributionClient/Main.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Chared; 
 using Newtonsoft.Json;
 
