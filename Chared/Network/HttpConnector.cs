70138f186768378b102b2f057a50a5c1b91a812b
John Larsen <john@pcis.se>
2016-09-08 17:14:40 +0200

    ok

--- Chared/Network/HttpConnector.cs
+++ Chared/Network/HttpConnector.cs
@@ -1,33 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace Chared.Network
 {
-	public class HttpConnector
-	{
-		/// <summary>
-		/// Takes a VersionInifo and get a Versionlist from server
-		/// </summary>
-		/// <param name="localVersion"></param>
-		/// <returns></returns>
-		public VersionList GetVersionList(VersionInfo localVersion)
-		{
-			string versionList; 
-			try
-			{
-				WebClient wc = new WebClient();
-				versionList = Path.Combine(localVersion.TempDir + "Versionlist.json"); 
-				wc.DownloadFile(localVersion.ServerList[0].Url, versionList);
-				wc.Dispose();
-			}
-			catch (Exception ex)
-			{
-				return null; 
-			}
-			string jsonText = File.ReadAllText(versionList);
-			return JsonConvert.DeserializeObject<VersionList>(jsonText);
-		}
-	}
+    class HttpConnector
+    {
+        public bool GetUpdateExist(UpdateAppInfo localAppVersion, int ServerListNumber)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadFile(localAppVersion.ServerList[ServerListNumber].Url, localAppVersion.TempDir + localAppVersion.FileName);
+                wc.Dispose();
+                var ServerAppVersion = GetObjectFromFile(Path.Combine(localAppVersion.TempDir, localAppVersion.FileName));
+                if(ServerAppVersion.ApllicationVersion > localAppVersion.ApllicationVersion)
+
+            }
+            catch (Exception ex)
+            {
+               // TODO loging in here 
+                return null; 
+            }
+        }
+
+        private UpdateAppInfo GetObjectFromFile(string file)
+        {
+            string jsonText = File.ReadAllText(file);
+            return JsonConvert.DeserializeObject<UpdateAppInfo>(jsonText);
+        }
+    }
 }
